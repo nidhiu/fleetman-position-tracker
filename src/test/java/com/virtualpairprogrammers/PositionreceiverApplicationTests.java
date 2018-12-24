63cbc4eb7952eeca0b52574b51f3e5bb62280e43
nidhiu <45939238+nidhiu@users.noreply.github.com>
2018-12-23 18:24:47 -0600

    Update PositionreceiverApplicationTests.java

--- src/test/java/com/virtualpairprogrammers/PositionreceiverApplicationTests.java
+++ src/test/java/com/virtualpairprogrammers/PositionreceiverApplicationTests.java
@@ -5,12 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class PositionreceiverApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	  assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
 	}
 
 }
